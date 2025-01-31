3a87de6541a5abf091617d48b3d5cc4e6431c93d
Isaac <45193261+isaacchunn@users.noreply.github.com>
2025-02-01 04:47:29 +0800

    fix: add node base path

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  assetPrefix: "/wanderers",
+  images : {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
