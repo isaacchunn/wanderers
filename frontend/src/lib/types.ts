23ebdb2c7c53ab13a2a7b3bf9cec35e09294cace
Jaysenso <jsenongxuan@gmail.com>
2025-02-12 16:03:54 +0800

    feat : Added googles place API integration in itinerary page

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -14,4 +14,45 @@ export interface Location {
     description: string;
     image: string;
     time: string;
+    placeDetails?: PlaceDetails;
+}
+
+export interface PlaceDetails {
+    place_id: string;
+    name: string;
+    formatted_address: string;
+    formatted_phone_number?: string;
+    international_phone_number?: string;
+    website?: string;
+    rating?: number;
+    user_ratings_total?: number;
+    price_level?: number; // 0 to 4
+    opening_hours?: {
+        weekday_text: string[];
+        open_now?: boolean;
+    };
+    photos?: {
+        photo_reference: string;
+        height: number;
+        width: number;
+    }[];
+    address_components: {
+        long_name: string;
+        short_name: string;
+        types: string[];
+    }[];
+    geometry: {
+        location: {
+            lat: number;
+            lng: number;
+        };
+    };
+    reviews?: {
+        author_name: string;
+        rating: number;
+        relative_time_description: string;
+        text: string;
+        profile_photo_url: string;
+    }[];
+    types?: string[];
 }
