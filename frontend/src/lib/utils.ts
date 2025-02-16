a02bcc210d2141fe8a9dee17a2a354e515f1e69b
Jaysenso <jsenongxuan@gmail.com>
2025-02-17 00:19:43 +0800

    fix : Removed unused var

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { Location, Itinerary, PlaceDetails } from "./types";
+import { Location, PlaceDetails } from "./types";
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
