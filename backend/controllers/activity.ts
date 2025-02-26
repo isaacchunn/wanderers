899f261b274bad618a7708913a59339941fdab20
Albert481 <albertyu481@gmail.com>
2025-02-26 13:06:40 +0800

    fix sonarqube 2

--- backend/controllers/activity.ts
+++ backend/controllers/activity.ts
@@ -18,7 +18,7 @@ import { HttpCode } from "../lib/httpCodes";
 // @route   POST /api/activity
 // @access  Private
 export const createActivityController = async (req: AuthenticatedRequest, res: Response) => {
-  let responseCode: number;
+  let responseCode = HttpCode.OK;
   let responseBody: any = {};
 
   try {
@@ -41,7 +41,7 @@ export const createActivityController = async (req: AuthenticatedRequest, res: R
     responseBody = { message: error.message || "Internal Server Error" };
   }
 
-  res.status(HttpCode.OK).json(responseBody);
+  res.status(responseCode).json(responseBody);
 };
 
 
