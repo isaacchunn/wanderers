e1b2aa607c78b0eee3f2f15d3b890c86e023116a
fattycuty <hazimgunz@gmail.com>
2025-03-06 15:21:48 +0800

    revert commit

--- backend/services/websocket-server.ts
+++ backend/services/websocket-server.ts
@@ -11,37 +11,37 @@ export const setupSocket = (server: http.Server) => {
     },
   });
 
-  // io.on("connection", (socket: Socket) => {
-  //   socket.on("joinRoom", (roomId: string) => {
-  //     socket.join(roomId); // Join room with the given roomId
+  io.on("connection", (socket: Socket) => {
+    socket.on("joinRoom", (roomId: string) => {
+      socket.join(roomId); // Join room with the given roomId
 
-  //     // Add to room trackingF
-  //     if (!rooms[roomId]) {
-  //       rooms[roomId] = new Set();
-  //     }
-  //     rooms[roomId].add(socket.id);
-  //   });
+      // Add to room trackingF
+      if (!rooms[roomId]) {
+        rooms[roomId] = new Set();
+      }
+      rooms[roomId].add(socket.id);
+    });
 
-  //   socket.on(
-  //     "sendMessage",
-  //     async (roomId: string, userId: string, message: string) => {
-  //       const createdChatMessage = await createMessage(
-  //         Number(userId),
-  //         Number(roomId),
-  //         message
-  //       );
-  //       io.to(roomId).emit("receiveMessage", createdChatMessage);
-  //     }
-  //   );
+    socket.on(
+      "sendMessage",
+      async (roomId: string, userId: string, message: string) => {
+        const createdChatMessage = await createMessage(
+          Number(userId),
+          Number(roomId),
+          message
+        );
+        io.to(roomId).emit("receiveMessage", createdChatMessage);
+      }
+    );
 
-  //   socket.on("disconnect", () => {
-  //     // Remove the user from the rooms
-  //     for (const roomId in rooms) {
-  //       if (rooms[roomId].has(socket.id)) {
-  //         rooms[roomId].delete(socket.id);
-  //       }
-  //     }
-  //   });
-  // });
+    socket.on("disconnect", () => {
+      // Remove the user from the rooms
+      for (const roomId in rooms) {
+        if (rooms[roomId].has(socket.id)) {
+          rooms[roomId].delete(socket.id);
+        }
+      }
+    });
+  });
   console.log("[socket]: io connected");
 };
