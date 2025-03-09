b87600250ecf73d92382ffd8fb5bb50de2edec58
fattycuty <hazimgunz@gmail.com>
2025-03-09 13:08:22 +0800

    SCEL-154: [FE] Update Chat box UI/UX (#75)

--- frontend/src/components/chat-box.tsx
+++ frontend/src/components/chat-box.tsx
@@ -1,15 +1,12 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { ChatMessage, Itinerary } from "@/lib/types";
 import { io } from "socket.io-client";
 import { toast } from "sonner";
-import { MessageCircle, X } from "lucide-react";
+import { MessageCircle } from "lucide-react";
 import { cn } from "@/lib/utils";
+import ChatCard from "./chat-card";
 
 const NEXT_PUBLIC_BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
@@ -72,13 +69,13 @@ export function ChatBox({
       });
 
       newSocket.on("disconnect", () => {
-        toast.error("Connection lost. Reconnecting...");
+        // toast.error("Connection lost. Reconnecting...");
         setIsConnectionActive(false);
         // Handle reconnection attempt
       });
 
       newSocket.io.on("reconnect_failed", () => {
-        toast.error("Connection lost. Please refresh the page.");
+        // toast.error("Connection lost. Please refresh the page.");
       });
 
       // Cleanup when the component unmounts
@@ -148,71 +145,42 @@ export function ChatBox({
     setNewMessage("");
   }
 
+  const groupedMessages = useMemo(() => {
+    return messages.reduce((acc, message) => {
+      const msgDate = new Date(message.created_at);
+      const today = new Date();
+      const yesterday = new Date();
+      yesterday.setDate(today.getDate() - 1);
+
+      let dateLabel =
+        msgDate.toDateString() === today.toDateString()
+          ? "Today"
+          : msgDate.toDateString() === yesterday.toDateString()
+          ? "Yesterday"
+          : msgDate.toLocaleDateString("en-GB");
+
+      acc[dateLabel] = acc[dateLabel] || [];
+      acc[dateLabel].push(message);
+      return acc;
+    }, {} as Record<string, typeof messages>);
+  }, [messages]);
+
   return (
     <>
       {!isMobile ? (
         <div ref={parentRef} className="relative">
-          <Card
-            className={`absolute transition-all duration-300 ease-in-outs`}
-            style={{ top: `${chatboxY}px` }}
-          >
-            <CardHeader>
-              <CardTitle>Group Chat</CardTitle>
-            </CardHeader>
-            <CardContent className="h-80 max-h-80 flex flex-col">
-              <ScrollArea className="max-h-80 flex-1 pr-4" type="always">
-                <div className="space-y-4 pt-1y">
-                  {messages.map((message) => (
-                    <div
-                      key={message.id}
-                      className="flex flex-col space-y-1 text-sm"
-                    >
-                      <div
-                        className={`flex items-center gap-2 ${
-                          message.chat_message_by_id.toString() ===
-                          userId.toString()
-                            ? "justify-end"
-                            : ""
-                        }`}
-                      >
-                        <span className="font-medium">
-                          {message.chat_message_by.username}
-                        </span>
-                        <span className="text-xs text-muted-foreground">
-                          {new Date(message.created_at).toLocaleTimeString()}
-                        </span>
-                      </div>
-                      <p
-                        className={`text-muted-foreground ${
-                          message.chat_message_by_id.toString() ===
-                          userId.toString()
-                            ? "text-right"
-                            : ""
-                        }`}
-                      >
-                        {message.chat_message}
-                      </p>
-                    </div>
-                  ))}
-                  {/* Auto scroll marker */}
-                  <div ref={messagesEndRef}></div>
-                </div>
-              </ScrollArea>
-              <form onSubmit={handleSubmit} className="mt-4 flex gap-2">
-                <Input
-                  placeholder={
-                    isConnectionActive ? "Type a message..." : "Connecting..."
-                  }
-                  value={newMessage}
-                  disabled={!isConnectionActive}
-                  onChange={(e) => setNewMessage(e.target.value)}
-                />
-                <Button type="submit" disabled={!isConnectionActive}>
-                  Send
-                </Button>
-              </form>
-            </CardContent>
-          </Card>
+          <ChatCard
+            groupedMessages={groupedMessages}
+            userId={userId}
+            isConnectionActive={isConnectionActive}
+            newMessage={newMessage}
+            handleSubmit={handleSubmit}
+            setNewMessage={setNewMessage}
+            messagesEndRef={messagesEndRef}
+            isMobile={isMobile}
+            chatboxY={chatboxY}
+            setIsChatBoxOpen={setIsChatBoxOpen}
+          />
         </div>
       ) : (
         <>
@@ -240,78 +208,18 @@ export function ChatBox({
                   "opacity 0.3s ease-in-out, transform 0.3s ease-in-out",
               }}
             >
-              <Card>
-                <CardHeader>
-                  <CardTitle>
-                    <div className="flex justify-between items-center rounded-t-lg">
-                      <h2 className="text-sm font-semibold">Chat</h2>
-                      <button
-                        onClick={() => setIsChatBoxOpen(false)}
-                        className="text-gray-500 hover:text-gray-700"
-                      >
-                        <X size={18} />
-                      </button>
-                    </div>
-                  </CardTitle>
-                </CardHeader>
-                <CardContent className="h-80 max-h-80 flex flex-col">
-                  <ScrollArea className="max-h-80 flex-1 pr-4" type="always">
-                    <div className="space-y-4 pt-1y">
-                      {messages.map((message) => (
-                        <div
-                          key={message.id}
-                          className="flex flex-col space-y-1 text-sm"
-                        >
-                          <div
-                            className={`flex items-center gap-2 ${
-                              message.chat_message_by_id.toString() ===
-                              userId.toString()
-                                ? "justify-end"
-                                : ""
-                            }`}
-                          >
-                            <span className="font-medium">
-                              {message.chat_message_by.username}
-                            </span>
-                            <span className="text-xs text-muted-foreground">
-                              {new Date(
-                                message.created_at
-                              ).toLocaleTimeString()}
-                            </span>
-                          </div>
-                          <p
-                            className={`text-muted-foreground ${
-                              message.chat_message_by_id.toString() ===
-                              userId.toString()
-                                ? "text-right"
-                                : ""
-                            }`}
-                          >
-                            {message.chat_message}
-                          </p>
-                        </div>
-                      ))}
-                      {/* Auto scroll marker */}
-                      <div ref={messagesEndRef}></div>
-                    </div>
-                  </ScrollArea>
-                  <form onSubmit={handleSubmit} className="mt-4 flex gap-2">
-                    <Input
-                      placeholder={
-                        isConnectionActive
-                          ? "Type a message..."
-                          : "Connecting..."
-                      }
-                      value={newMessage}
-                      disabled={!isConnectionActive}
-                      onChange={(e) => setNewMessage(e.target.value)}
-                    />
-                    <Button type="submit" disabled={!isConnectionActive}>
-                      Send
-                    </Button>
-                  </form>
-                </CardContent>
-              </Card>
+              <ChatCard
+                groupedMessages={groupedMessages}
+                userId={userId}
+                isConnectionActive={isConnectionActive}
+                newMessage={newMessage}
+                handleSubmit={handleSubmit}
+                setNewMessage={setNewMessage}
+                messagesEndRef={messagesEndRef}
+                isMobile={isMobile}
+                chatboxY={chatboxY}
+                setIsChatBoxOpen={setIsChatBoxOpen}
+              />{" "}
             </div>
           )}
         </>
